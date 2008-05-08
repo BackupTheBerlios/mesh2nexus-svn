7d5893d84739850e10a407f8eee84d28b84bba33
nexus2mesh <nexus2mesh@67a7065e-733e-0410-9dd6-b6656a2b1ce0>
2008-05-08 10:58:28 +0000

    rmi test

--- CSAVA_Client/src/client/ClientStart.java
+++ CSAVA_Client/src/client/ClientStart.java
@@ -1,9 +1,45 @@
 package client;
 
+import java.net.MalformedURLException;
+import java.rmi.*;
+
+import communication.ServerInterface;
+
 public class ClientStart {
 	
+	public static String ServerURL;
+	public static ServerInterface server;
 	
 	
+	public static void main(String[] args){
+		
+		
+		// Start Verbindungsdialog
+		// Serverdaten auslesen
+		// Verbindung zum Server aufbauen
+		// Start Haupt-Gui
+		
+		
+		ServerURL =  "92.227.33.232:21";
+		
+		try {
+			server = (ServerInterface) Naming.lookup("rmi://"+ ServerURL +"/ServerFunctions");
+			
+			System.out.println(server.getString());
+			
+		} catch (MalformedURLException e) {
+			// TODO
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 	
 	
