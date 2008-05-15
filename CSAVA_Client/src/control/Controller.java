58a7d6b34ecb228f0f38bc2102e106470b599e3c
nexus2mesh <nexus2mesh@67a7065e-733e-0410-9dd6-b6656a2b1ce0>
2008-05-15 15:16:59 +0000

    ST: Tabelle wird jetzt richtig gefuelt!

--- CSAVA_Client/src/control/Controller.java
+++ CSAVA_Client/src/control/Controller.java
@@ -22,165 +22,153 @@ import gui.MainGUI;
 import gui.QueryGUI;
 
 /**
- * @author 
- *
+ * @author
+ * 
  */
 public class Controller {
-	
+
 	public static String ServerURL;
 	public static ServerInterface server;
-	
+
 	public static LogonGUI logonGui;
 	public static MainGUI mainGui;
-	public static QueryGUI queryGui;	
+	public static QueryGUI queryGui;
 	public static ErrorDialog errDialog;
 	public static boolean connect = false;
 
 	/**
-	 * @param 
+	 * @param
 	 */
-//	public static String getServerURL() {
-//		
-//		return logonGui.text.getText();
-//		//return "ura";
-//	}
-
-	public static void StartLogonGui() {		
-		logonGui = new LogonGUI();		
+	// public static String getServerURL() {
+	//		
+	// return logonGui.text.getText();
+	// //return "ura";
+	// }
+	public static void StartLogonGui() {
+		logonGui = new LogonGUI();
 	}
 
-	public static void ShowErrorDialog() {		
-		errDialog = new ErrorDialog();		
-		
+	public static void ShowErrorDialog() {
+		errDialog = new ErrorDialog();
 
 	}
 
-//	public static void setServerURL(String text) {
-//		ClientStart.ServerURL = text;
-//		
-//	}
-	
+	// public static void setServerURL(String text) {
+	// ClientStart.ServerURL = text;
+	//		
+	// }
 
 	public static String ConnectToServer(String text) {
-		
+
 		String response = "Es ist ein Fehler aufgetreten";
-		// Serverdaten auslesen	
+		// Serverdaten auslesen
 		ServerURL = text;
-		
+
 		// Verbindung zum Server aufbauen
 		try {
-			server = (ServerInterface) Naming.lookup("rmi://"+ ServerURL +"/ServerFunctions");
+			server = (ServerInterface) Naming.lookup("rmi://" + ServerURL
+					+ "/ServerFunctions");
 			// Get response
 			response = server.getString();
 			System.out.println(response);
 			connect = true;
 			return response;
-			
+
 		} catch (MalformedURLException e) {
 			//  
 			e.printStackTrace();
 		} catch (RemoteException e) {
 			// TODO: Parameter fuer Dialog uebergeben!
-			//ShowErrorDialog();
+			// ShowErrorDialog();
 			e.printStackTrace();
 		} catch (NotBoundException e) {
 			//  
 			e.printStackTrace();
 		}
-		
-		return response ;
-		
-		
+
+		return response;
+
 	}
 
 	public static void StartMainGui() {
-		mainGui = new MainGUI();	
-		
+		mainGui = new MainGUI();
+
 	}
 
 	public static void StartQueryGui() {
-		queryGui = new QueryGUI();	
-		
+		queryGui = new QueryGUI();
+
 	}
-	
+
 	public static JCO.Table CreateTable(String ura) {
-		
+
 		JCO.Table sales_orders = null;
 		try {
 			// RPC!
-			sales_orders = server.getSalesOrderList("0000100001", "WING", "2005-05-28", "2005-05-30", "0");
-			
+			sales_orders = server.getSalesOrderList("0000100001", "WING",
+					"2005-05-28", "2005-05-30", "0");
+
 			if (sales_orders == null) {
-				System.err.println("Funktion konnte nicht ausgeführt werden, siehe Server Log");
-			}
-			else {
+				System.err
+						.println("Funktion konnte nicht ausgeführt werden, siehe Server Log");
+			} else {
 				System.out.println(sales_orders.getNumRows());
-				//return sales_orders;
+				// return sales_orders;
 			}
 
 		} catch (RemoteException e) {
 			// 
 			e.printStackTrace();
 		}
-		// Valeri4 eto 4ish dage ne smotri)))))))))))))))))
-		
+
 		// Spalten
-		
-	           for (JCO.FieldIterator e = sales_orders.fields(); e.hasMoreElements(); ) {
-	        	   JCO.Field field = e.nextField();
-	               // Output
-	               //System.out.println(field.getName() + ":\t" + field.getString());
-	      		
-	               TableColumn id = new TableColumn(MainGUI.table,SWT.LEFT);				
-	               id.setText(field.getName());				
-	               id.setWidth(150);
-	      		   
-	            }//for
 
+		for (JCO.FieldIterator e = sales_orders.fields(); e.hasMoreElements();) {
+			JCO.Field field = e.nextField();
+			// Create Column
+			TableColumn id = new TableColumn(MainGUI.table, SWT.LEFT);
+			id.setText(field.getName());
+			id.setWidth(100);
+		}
 
-		
+		//
+		System.out.println("Anzahl von Spalten" + ":\t"
+				+ MainGUI.table.getColumnCount());
+
 		// Zeilen
-	           String[] items = new String[MainGUI.table.getColumnCount()];
-	           int i = 0;
+		int i;
 		do {
-           System.out.println("-----------------------------------------");
-           //
-           //TableItem item1 = new TableItem(MainGUI.table,SWT.NONE);
-           // Loop over all columns in the current row
-           for (JCO.FieldIterator e = sales_orders.fields(); e.hasMoreElements(); ) {
-        	   JCO.Field field = e.nextField();
-               // Output
-               System.out.println(field.getName() + ":\t" + field.getString());
-      		items[i] = field.getString();  
-      		i++;
-      		
-      		
-      		TableItem item1 = new TableItem(MainGUI.table,SWT.NONE);
-            item1.setText(field.getString() + i);
-      		   
-            }//for
-           //item1.setText(items);
-           
-        } while(sales_orders.nextRow());
+			System.out.println("-----------------------------------------");
+			//
+			i = 0;
+			TableItem item1 = new TableItem(MainGUI.table, SWT.NONE);
+			// Loop over all columns in the current row
+			for (JCO.FieldIterator e = sales_orders.fields(); e
+					.hasMoreElements();) {
+				JCO.Field field = e.nextField();
 
-		//sales_orders.writeXML(arg0);
+				// Output
+				System.out.println(field.getName() + ":\t" + field.getString());
+
+				item1.setText(i, field.getString());
+				i++;
+
+			}
+		} while (sales_orders.nextRow());
+
 		return sales_orders;
 	}
 
 	public static void CloseLogonGui() {
-		//LogonGUI.display.getActiveShell().dispose();
-		//LogonGUI.display.getActiveShell().close();
-		
-		
+		// LogonGUI.display.getActiveShell().dispose();
+		// LogonGUI.display.getActiveShell().close();
+
 	}
 
 	public static void CloseQueryGui() {
-		
+
 		LogonGUI.display.getActiveShell().close();
-		
+
 	}
-
-
-
 
 }
